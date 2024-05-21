cdf64d94a22de9ef59c92836e01e98e4d8c92031
Omar Sierra <oysierra@gmail.com>
2024-05-20 19:31:15 -0500

    Code corrected

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; // Importar estilos si es necesario
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Table from './components/Table/Table';
@@ -8,20 +8,42 @@ import Button from './components/Button/Button';
 import { colorEvenRows, colorOddRows, filterRecords, filterTable } from './utils';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div className="buttons">
-        <Button onClick={colorEvenRows} text="Color Pares" />
-        <Button onClick={colorOddRows} text="Color Impares" />
-        <Button onClick={() => filterRecords(1, 10)} text="Filtrar 1-10" />
-        <Button onClick={() => filterRecords(11, 20)} text="Filtrar 11-20" />
-      </div>
-      <SearchInput onKeyUp={filterTable} placeholder="Buscar..." />
-      <Table />
-      <Footer />
-    </div>
-  );
+    const [tableRows, setTableRows] = useState([]);
+    
+    // Funciones para manejar los eventos de los botones
+    const handleColorEvenRows = () => {
+        colorEvenRows(tableRows);
+    };
+
+    const handleColorOddRows = () => {
+        colorOddRows(tableRows);
+    };
+
+    const handleFilterRecords1To10 = () => {
+        const filteredRows = filterRecords(tableRows, 1, 10);
+        setTableRows(filteredRows);
+    };
+
+    const handleFilterRecords11To20 = () => {
+        const filteredRows = filterRecords(tableRows, 11, 20);
+        setTableRows(filteredRows);
+    };
+
+    return (
+        <div className="App">
+            <Header />
+            <div className="buttons">
+                <Button onClick={handleColorEvenRows} label="Color Pares" />
+                <Button onClick={handleColorOddRows} label="Color Impares" />
+                <Button onClick={handleFilterRecords1To10} label="Filtrar 1-10" />
+                <Button onClick={handleFilterRecords11To20} label="Filtrar 11-20" />
+            </div>
+            <SearchInput onKeyUp={filterTable} placeholder="Buscar..." />
+            <Table />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
+
